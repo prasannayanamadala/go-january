618d6c54ec8978b23018d25977fb05361b50455f
prasannaraavi <raaviprasanna9@gmail.com>
2022-01-10 13:48:37 -0500

    changes

--- arrays/array.go
+++ arrays/array.go
@@ -12,17 +12,17 @@ func ArrayDefine() {
 	fmt.Println("grade1:",grade1)
 	fmt.Println("grade2:",grade2)
 	fmt.Println("grade3:",grade3)
-	fmt.Println("grade1:",&grade1)
-	fmt.Println("grade2:",&grade2)
-	fmt.Println("grade3:",&grade3) //& gives address of a particular variable
+	fmt.Println("grade1 address:",&grade1)
+	fmt.Println("grade2 address:",&grade2)
+	fmt.Println("grade3 address:",&grade3) //& gives address of a particular variable
 
 	//firstway:shorthand declaration in an array
 
 	grades := [3]int{grade1, grade2, grade3} //grades := [...]int{grade1, grade2, grade3}
 	fmt.Println("grades in an array:",grades)
-	fmt.Println("grade1:", &grades[0])
-	fmt.Println("grade2:", &grades[1])
-	fmt.Println("grade3:", &grades[2])
+	fmt.Println("grade1 address:", &grades[0])
+	fmt.Println("grade2 address:", &grades[1])
+	fmt.Println("grade3 address:", &grades[2])
 	
 	firstgrade := grades[0]    //for accessing one value.Arrays are zero indexed
 	fmt.Println("first grade representation in an array:",firstgrade)
@@ -59,5 +59,6 @@ arraygit1 := arraygit
 arraygit[0] = "sub" //changing the value of one array 
 fmt.Println("string array:",arraygit1)
 fmt.Println("string array:",arraygit)
+fmt.Println("string array length:",len(arraygit))
 
 }
