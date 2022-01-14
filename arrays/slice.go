87e884ef80073de00f0742fff8d44718bbf57157
prasannaraavi <raaviprasanna9@gmail.com>
2022-01-13 22:46:42 -0500

    slices

--- arrays/slice.go
+++ arrays/slice.go
@@ -10,20 +10,29 @@ func SliceDefine() {
 	fmt.Println(slices)   //0, 1, 2, 3, 4, 5
 	
 	slices = slices[2:6] //second element to fifth(6-1) element
-	fmt.Println(slices)
+	fmt.Println(slices)   //30,40,50,60
 
-	slices1 := slices
-	slices[2] = 333
-	fmt.Println("slices1:",slices1)
+	slice1 := slices
+	slices[2] = 333     //30,40,333,60
+	fmt.Println("slice1:",slice1)
+	fmt.Println(slices) //30,40,333,60
 
 	slices = append(slices,110,120,130,140)
-	fmt.Println(slices)
+	fmt.Println(slices) //30,40,333,60,110,120,130,140
 	
-	fmt.Println("length of slices:",len(slices))
-	fmt.Println("Address of slice 10:",&(slices[10]))
-	fmt.Println("capacity of slices:",cap(slices))
-	
+	fmt.Println("length of slices:",len(slices))  //8
+	fmt.Println("capacity of slices:",cap(slices)) 
+
+	slice2 := slices[2:]
+	fmt.Println("slice2:",slice2) // 333,60,110,120,130,140
 
 	slices = append(slices,150,160,170,180)
-	fmt.Println(slices)
+	fmt.Println(slices) //30,40,333,60,110,120,130,140,150,160,170,180
+
+	slice3 := slices[:7]
+	fmt.Println("slice3:",slice3) // 30,40,333,60,110,120,130
+
+	slices[4] = 500
+	fmt.Println(slices) //30,40,333,60,500,120,130,140,150,160,170,180
+	
 	}
