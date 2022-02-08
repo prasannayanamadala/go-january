d2bb9c748695d2b3e66e48956a2dd266a2c28f2e
prasannaraavi <raaviprasanna9@gmail.com>
2022-02-08 14:33:38 -0500

    go-routines

--- goroutines/goroutines.go
+++ goroutines/goroutines.go
@@ -10,18 +10,24 @@ var wg = sync.WaitGroup{}
 
 func GoRoutineTest() {
 	fmt.Println("testing of goroutine")
+	x := 1
+	for i:=0; i<5 ; i++ {
+	
 	wg.Add(2)
-	go ParallelismTest()
-	go NextFunc()
+	go ParallelismTest(&x)
+	go NextFunc(&x)
 	wg.Wait()
+	}
 	//time.Sleep(2 * time.Millisecond)
 }
-func ParallelismTest() {
+func ParallelismTest(a *int) {
 	//time.Sleep(2 * time.Millisecond)
-	fmt.Println("Does it print on time")
+	*a = *a +1
+	fmt.Println("1st func:",*a)
 	wg.Done()
 }
-func NextFunc() {
-	fmt.Println("think so,it can print on time")
+func NextFunc(a *int) {
+	*a = *a +1
+	fmt.Println("2nd func:",*a)
 	wg.Done()
 }
