6037eeb19c899c2548d798045778e2ca63107c96
prasannaraavi <raaviprasanna9@gmail.com>
2022-02-10 13:32:38 -0500

    channels

--- goroutines/goroutines.go
+++ goroutines/goroutines.go
@@ -11,23 +11,34 @@ var wg = sync.WaitGroup{}
 func GoRoutineTest() {
 	fmt.Println("testing of goroutine")
 	x := 1
-	for i:=0; i<5 ; i++ {
+ 	//for i:=0; i<5 ; i++ {
 	
-	wg.Add(2)
-	go ParallelismTest(&x)
-	go NextFunc(&x)
-	wg.Wait()
-	}
-	//time.Sleep(2 * time.Millisecond)
+// 	wg.Add(2)
+// 	go ParallelismTest(&x)
+// 	go NextFunc(&x)
+// 	wg.Wait()
+// 	}
+// 	//time.Sleep(2 * time.Millisecond)
+// }
+// func ParallelismTest(a *int) {
+// 	//time.Sleep(2 * time.Millisecond)
+// 	*a = *a +1
+// 	fmt.Println("1st func:",*a)
+// 	wg.Done()
+// }
+// func NextFunc(a *int) {
+// 	*a = *a +1
+// 	fmt.Println("2nd func:",*a)
+// 	wg.Done()
+// }
+ch := make(chan int,1)
+ch <-x
+wg.Add(1)
+go SampleGoroutine(ch)
+wg.Wait()
 }
-func ParallelismTest(a *int) {
-	//time.Sleep(2 * time.Millisecond)
-	*a = *a +1
-	fmt.Println("1st func:",*a)
-	wg.Done()
-}
-func NextFunc(a *int) {
-	*a = *a +1
-	fmt.Println("2nd func:",*a)
+func SampleGoroutine(ch chan int) {
+	val := <-ch
+	fmt.Println(val)
 	wg.Done()
 }
