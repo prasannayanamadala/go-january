b43e99c4b48466d551979c38f57a777e6550be83
prasannaraavi <raaviprasanna9@gmail.com>
2022-02-22 15:11:02 -0500

    1

--- api/api.go
+++ api/api.go
@@ -22,7 +22,8 @@ var albums = []album{
 func main() {
     router := gin.Default()
     router.GET("/albums", getAlbums)
-
+	router.GET("/albums/:id", getAlbumByID)
+    router.POST("/albums", postAlbums)
     router.Run("localhost:8080")
 }
 // getAlbums responds with the list of all albums as JSON
